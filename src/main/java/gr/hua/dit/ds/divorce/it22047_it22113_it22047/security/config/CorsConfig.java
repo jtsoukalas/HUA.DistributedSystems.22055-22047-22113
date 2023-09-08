15a514b49c465f8cc69cc69a8d5f7284aa319a43
Jason Tsoukalas <jasontsoukalas@icloud.com>
2023-09-09 00:31:15 +0300

    Cors config addition

---
+++ src/main/java/gr/hua/dit/ds/divorce/it22047_it22113_it22047/security/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package gr.hua.dit.ds.divorce.it22047_it22113_it22047.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Allow requests from any origin
+        configuration.addAllowedMethod("*"); // Allow all HTTP methods
+        configuration.addAllowedHeader("*"); // Allow all headers
+        configuration.setAllowCredentials(true); // Allow credentials (e.g., cookies)
+        configuration.setMaxAge(3600L); // Max age of the CORS options response
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply CORS rules to all paths
+
+        return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+}
