3cb0e4bdc8a812abcf3afd6d356ad2607e0d5876
Jason Tsoukalas <jasontsoukalas@icloud.com>
2023-09-19 22:33:33 +0300

    .

--- src/main/java/gr/hua/dit/ds/divorce/it22047_it22113_it22047/entity/Role.java
+++ src/main/java/gr/hua/dit/ds/divorce/it22047_it22113_it22047/entity/Role.java
@@ -6,10 +6,30 @@ public enum Role implements GrantedAuthority {
         SPOUSE,
         LAWYER,
         NOTARY,
-        ADMIN;
+        ADMIN,
+        SPOUSE_ADMIN,
+        SPOUSE_LAWYER,
+        SPOUSE_NOTARY;
+
 
         @Override
         public String getAuthority() {
-                return this.name();
+                switch (this) {
+                        case SPOUSE:
+                                return "ROLE_SPOUSE";
+                        case LAWYER:
+                                return "ROLE_LAWYER";
+                        case NOTARY:
+                                return "ROLE_NOTARY";
+                        case ADMIN:
+                                return "ROLE_ADMIN";
+                        case SPOUSE_ADMIN:
+                                return "ROLE_SPOUSE_ADMIN";
+                        case SPOUSE_LAWYER:
+                                return "ROLE_SPOUSE_LAWYER";
+                        case SPOUSE_NOTARY:
+                                return "ROLE_SPOUSE_NOTARY";
+                }
+                return null;
         }
 }
